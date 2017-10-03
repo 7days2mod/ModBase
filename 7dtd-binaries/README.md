586cc037069a542ec2ab9eb702609180690e31e2
StompyNZ <russell@codestorm.nz>
2017-10-03 16:32:32 +1300

    1.0

---
+++ 7dtd-binaries/README.md
@@ -0,0 +1,7 @@
+For compiling the source code you will need to use one of the options below
+
+1. Put the below files from your dedicated server into this folder:
+* Assembly-CSharp.dll
+* Any other dll files from the '7DaysToDieServer_Data/Managed' folder that you use
+
+2. Alternatively you can update the project references to point to the dlls in your dedi '7DaysToDieServer_Data/Managed' folder.
---
+++ ModBase/API.cs
@@ -0,0 +1,60 @@
+﻿namespace ModBase
+{
+  public class API : ModApiAbstract
+  {
+    public override void CalcChunkColorsDone(Chunk _chunk)
+    {
+      base.CalcChunkColorsDone(_chunk);
+    }
+
+    public override bool ChatMessage(ClientInfo _cInfo, EnumGameMessages _type, string _msg, string _mainName, bool _localizeMain, string _secondaryName, bool _localizeSecondary)
+    {
+      return base.ChatMessage(_cInfo, _type, _msg, _mainName, _localizeMain, _secondaryName, _localizeSecondary);
+    }
+
+    public override void GameAwake()
+    {
+      base.GameAwake();
+    }
+
+    public override void GameShutdown()
+    {
+      base.GameShutdown();
+    }
+
+    public override void GameStartDone()
+    {
+      base.GameStartDone();
+    }
+
+    public override void GameUpdate()
+    {
+      base.GameUpdate();
+    }
+
+    public override void PlayerDisconnected(ClientInfo _cInfo, bool _bShutdown)
+    {
+      base.PlayerDisconnected(_cInfo, _bShutdown);
+    }
+
+    public override void PlayerLogin(ClientInfo _cInfo, string _compatibilityVersion)
+    {
+      base.PlayerLogin(_cInfo, _compatibilityVersion);
+    }
+
+    public override void PlayerSpawnedInWorld(ClientInfo _cInfo, RespawnType _respawnReason, Vector3i _pos)
+    {
+      base.PlayerSpawnedInWorld(_cInfo, _respawnReason, _pos);
+    }
+
+    public override void PlayerSpawning(ClientInfo _cInfo, int _chunkViewDim, PlayerProfile _playerProfile)
+    {
+      base.PlayerSpawning(_cInfo, _chunkViewDim, _playerProfile);
+    }
+
+    public override void SavePlayerData(ClientInfo _cInfo, PlayerDataFile _playerDataFile)
+    {
+      base.SavePlayerData(_cInfo, _playerDataFile);
+    }
+  }
+}
