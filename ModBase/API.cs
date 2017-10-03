8447cbb3a80ffd84c66e12aa6f123c2d9d3adc96
StompyNZ <russell@codestorm.nz>
2017-10-03 16:58:17 +1300

    1.0

--- ModBase/API.cs
+++ ModBase/API.cs
@@ -4,57 +4,61 @@
   {
     public override void CalcChunkColorsDone(Chunk _chunk)
     {
-      base.CalcChunkColorsDone(_chunk);
+      //runs each time a chunk has it's colors re-calculated. For example this is used to generate the images for allocs game map mod
     }
 
     public override bool ChatMessage(ClientInfo _cInfo, EnumGameMessages _type, string _msg, string _mainName, bool _localizeMain, string _secondaryName, bool _localizeSecondary)
     {
-      return base.ChatMessage(_cInfo, _type, _msg, _mainName, _localizeMain, _secondaryName, _localizeSecondary);
+      //return true to pass the message on to the next mod, or if no other mods then it will output to chat.
+      //return false to prevent the message from being passed on or output to chat
+      return true;
     }
 
     public override void GameAwake()
     {
-      base.GameAwake();
+      //runs once when the server is ready for interaction and GameManager.Instance.World is set
     }
 
     public override void GameShutdown()
     {
-      base.GameShutdown();
+      //runs once when the server is about to shut down
     }
 
     public override void GameStartDone()
     {
-      base.GameStartDone();
+      //runs once when the server is ready for players to join
     }
 
     public override void GameUpdate()
     {
-      base.GameUpdate();
+      //runs any time the game executes an update (which is very often).
+      //place any tasks that you want to process in the main thread here with an execution rate limiter (such as creating entities via the entity factory)
     }
 
     public override void PlayerDisconnected(ClientInfo _cInfo, bool _bShutdown)
     {
-      base.PlayerDisconnected(_cInfo, _bShutdown);
+      //runs on each player disconnect
     }
 
     public override void PlayerLogin(ClientInfo _cInfo, string _compatibilityVersion)
     {
-      base.PlayerLogin(_cInfo, _compatibilityVersion);
+      //runs on initial connection from a player, _cInfo is usually null at this point
     }
 
     public override void PlayerSpawnedInWorld(ClientInfo _cInfo, RespawnType _respawnReason, Vector3i _pos)
     {
-      base.PlayerSpawnedInWorld(_cInfo, _respawnReason, _pos);
+      //runs each time a player spawns, including on login, respawn from death, and teleport
     }
 
     public override void PlayerSpawning(ClientInfo _cInfo, int _chunkViewDim, PlayerProfile _playerProfile)
     {
-      base.PlayerSpawning(_cInfo, _chunkViewDim, _playerProfile);
+      //runs just before a player is spawned int the world
     }
 
     public override void SavePlayerData(ClientInfo _cInfo, PlayerDataFile _playerDataFile)
     {
-      base.SavePlayerData(_cInfo, _playerDataFile);
+      //runs each time a player file is saved from the client to the server
+      //this will usually run about every 30 seconds per player as well as triggered updates such as dying
     }
   }
 }
